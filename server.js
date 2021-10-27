5e4d9b4f151c5b60756fa2aea840ba06608e491c
Abdul2067 <abdul2067@gmail.com>
2021-10-26 20:19:00 -0400

    add views directory

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res) {
-  res.send("")
+  res.redirect("/home")
+})
+
+app.get("/home", function(req, res) {
+  res.send("<h1>Home page</h1>")
 })
 
 app.listen(3000, function() {
